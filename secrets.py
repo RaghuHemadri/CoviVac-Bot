1c1c4d25c38d0a59f3e74356e6df822d6936a316
RaghuHemadri <45795135+RaghuHemadri@users.noreply.github.com>
2021-08-28 21:16:48 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,6 +1,6 @@
 # this is the bot token, gotten from BotFather
-TELEGRAM_BOT_TOKEN = "1869941801:AAHIPnZ1yTEeEa9PgP-t-tjHQ2gyqWd6PPs"
+TELEGRAM_BOT_TOKEN = ""
 # this is your own telegram ID, which is used to send exception tracebacks
-DEVELOPER_CHAT_ID = "@raghu_hemadri"
+DEVELOPER_CHAT_ID = ""
 # list of telegram IDs for them /stats command is enabled
 MAINTAINERS_CHAT_IDS = [DEVELOPER_CHAT_ID, ]
